bbdbac4de4169165265d3a687192f957dfaf8111
byronprentice <byron.prentice@digital.hmrc.gov.uk>
2017-10-13 10:42:28 +0100

    NGC-2413 reverted shuttering implementation, due to restrictions on the platform this feature doesn't behave as expected.

--- app/uk/gov/hmrc/ngc/orchestration/controllers/error.scala
+++ app/uk/gov/hmrc/ngc/orchestration/controllers/error.scala
@@ -27,5 +27,3 @@ case object ErrorBadRequest extends ErrorResponse(400, "BAD_REQUEST", "Invalid P
 case object MandatoryResponse extends ErrorResponse(500, "MANDATORY", "Mandatory data not found")
 
 case object ForbiddenAccess extends ErrorResponse(403, "UNAUTHORIZED", "Access denied!")
-
-case class ShutteredResponse(override val message: String) extends ErrorResponse(503, "SCHEDULED_MAINTENANCE", message)
